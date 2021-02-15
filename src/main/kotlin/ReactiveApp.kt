a7386fa040d9593193184dc490223e4c9f9a26ab
Shivku <sivakumar.sekar@rakuten.com>
2021-02-15 12:37:08 +0530

    Initialized main.

---
+++ src/main/kotlin/ReactiveApp.kt
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReactiveApp {
+    companion object {
+        @JvmStatic
+        public fun main(args: Array<String>) {
+            SpringApplication.run(ReactiveApp::class.java, *args)
+        }
+    }
+}

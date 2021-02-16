ab704f2b9189959e442ec5b67fa03ae23b59a986
Shivku <sivakumar.sekar@rakuten.com>
2021-02-17 01:54:34 +0530

    Added udpate end-point. Made get and getall use db.

--- src/main/kotlin/com/reactive/webapp/controller/EmployeeController.kt
+++ src/main/kotlin/com/reactive/webapp/controller/EmployeeController.kt
@@ -2,11 +2,11 @@ package com.reactive.webapp.controller
 
 import com.reactive.webapp.entities.Employee
 import com.reactive.webapp.service.EmployeeService
-import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.http.ResponseEntity
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import java.net.URI
 
 @RestController
@@ -21,12 +21,14 @@ class EmployeeController(private val employeeService: EmployeeService) {
 
     @GetMapping("/{id}")
     public suspend fun get(@PathVariable("id") id: Int): ResponseEntity<Employee> {
-        return ResponseEntity.ok(employees.first { employee -> employee.id == id })
+        val fetchedEmployee = employeeService.getById(id).awaitSingle()
+        return ResponseEntity.ok(fetchedEmployee)
     }
 
     @GetMapping
-    public suspend fun getAll(): ResponseEntity<List<Employee>> {
-        return ResponseEntity.ok(employees)
+    public suspend fun getAll(): ResponseEntity<Flux<Employee>> {
+        val fetchedEmployees = employeeService.getAll()
+        return ResponseEntity.ok(fetchedEmployees)
     }
 
     @PostMapping
@@ -36,5 +38,15 @@ class EmployeeController(private val employeeService: EmployeeService) {
     ): ResponseEntity<Unit> {
         val createdEmployee = employeeService.create(employee).awaitSingle()
         return ResponseEntity.created(URI("${request.uri}/${createdEmployee.id}")).build()
+    }
+
+    @PutMapping("/{id}")
+    public suspend fun update(
+        @PathVariable("id") id: Int,
+        @RequestBody employee: Employee,
+        request: ServerHttpRequest
+    ): ResponseEntity<Unit> {
+        employeeService.update(employee).awaitSingle()
+        return ResponseEntity.noContent().build()
     }
 }
--- src/main/kotlin/com/reactive/webapp/service/EmployeeService.kt
+++ src/main/kotlin/com/reactive/webapp/service/EmployeeService.kt
@@ -3,6 +3,7 @@ package com.reactive.webapp.service
 import com.reactive.webapp.entities.Employee
 import com.reactive.webapp.repository.EmployeeRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
@@ -12,6 +13,8 @@ class EmployeeService(private val employeeRepository: EmployeeRepository) {
     }
 
     fun getById(employeeId: Int): Mono<Employee> = employeeRepository.findEmployeeById(employeeId)
+
+    fun getAll(): Flux<Employee> = employeeRepository.findAll()
 
     fun update(employee: Employee): Mono<Employee> = employeeRepository.save(employee)
 }
